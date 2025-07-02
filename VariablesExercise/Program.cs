5e1501dc63879537857ffd400d4e0b25f7e97757
Cecelia Coombs <henyacc09@gmail.com>
2025-07-02 11:34:39 -0400

    completed class exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -2,9 +2,18 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string myName = "Cecelia";
+            int age = 40;
+            double avg = 98.7;
+            decimal bankAccount = 10000.00m;
+            char initial - 'C';
+            bool isMarried = true;
+
+            Console.WriteLine($"Name; {myName}\nAge): {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried}");
         }
+
     }
 }
