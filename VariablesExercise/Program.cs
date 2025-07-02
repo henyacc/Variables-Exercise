b052d42bca325fd4235974e128d3ce680990f068
Cecelia Coombs <henyacc09@gmail.com>
2025-07-02 11:38:12 -0400

    fix equal sign for my char

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -9,7 +9,7 @@
             int age = 40;
             double avg = 98.7;
             decimal bankAccount = 10000.00m;
-            char initial - 'C';
+            char initial = 'C';
             bool isMarried = true;
 
             Console.WriteLine($"Name; {myName}\nAge): {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried}");
